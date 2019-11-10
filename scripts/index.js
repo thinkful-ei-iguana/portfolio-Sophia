88c1c60282fba57da609a08adac1c4f5fed18a0c
Sophia Koeut <koeutsophia@gmail.com>
2019-11-09 22:14:56 -0800

    fixed navbar and mobile img re-sizing

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
-    $(".drop-down").hover(function(){
-      $(".menu", this).slideDown(100);
-    }, function(){
-      $(".menu", this).slideUp(100);
+let active = false;
+
+$(document).ready(function () {
+  //mobile view only
+  if ($(window).width() <= 400){	
+		$(".hamburger").click(function () {
+      toggleMenu();
     });
-  })
+    $(".menu li").click(function (){
+      toggleMenu();
+    })
+	}	
+});
+
+const toggleMenu = function () {
+  if (active === false) {
+    $('.menu').show();
+    $('.menu').addClass('menu-open');
+    active = true;
+  }
+  else {
+    $('.menu').hide();
+    $('.menu').removeClass('menu-open');
+    active = false;
+  }
+}
